41d08dcf4362e03f11a136833cefe9ed2c4d1c76
WiggelMc <78086343+WiggelMc@users.noreply.github.com>
2026-04-28 00:55:39 +0200

    Add actual data into behaviour

--- zmk-modules/tapithium-core/src/behaviors/behavior_tapithium_mods.c
+++ zmk-modules/tapithium-core/src/behaviors/behavior_tapithium_mods.c
@@ -7,9 +7,12 @@
 #define DT_DRV_COMPAT zmk_behavior_tapithium_mods
 
 // Dependencies
+#include <drivers/behavior.h>
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
-#include <drivers/behavior.h>
+
+#include <zmk/keymap.h>
+#include <zmk/keys.h>
 
 #include <dt-bindings/zmk/tapithium_mods.h>
 
@@ -19,62 +22,83 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
-// Instance-specific Data struct (Optional)
+typedef uint8_t zmk_tp_stage_t;
+#define TP_STAGE_IDLE 0
+#define TP_STAGE_MODS_SELECT 1
+#define TP_STAGE_MODS_ON 2
+
+typedef uint8_t zmk_tp_mode_t;
+#define TP_MODE_ENABLE 0
+#define TP_MODE_STICKY 1
+
 struct behavior_tapithium_mods_data {
-    bool data_param1;
-    bool data_param2;
-    bool data_param3;
+  bool is_active;
 };
 
-// Instance-specific Config struct (Optional)
 struct behavior_tapithium_mods_config {
-    bool config_param1;
-    bool config_param2;
-    bool config_param3;
+  zmk_keymap_layers_state_t mod_layers;
+  int32_t cancel_after_idle_ms;
 };
 
-// Initialization Function (Optional)
-static int tapithium_mods_init(const struct device *dev) {
-    return 0;
+struct behavior_tapithium_mods_engine_data {
+  zmk_tp_stage_t stage;
+  zmk_tp_mode_t mode;
+  struct behavior_tapithium_mods_config *config;
+  zmk_keymap_layer_index_t pending_layer;
+  zmk_mod_flags_t pending_mods;
 };
 
-static int on_tapithium_mods_binding_pressed(struct zmk_behavior_binding *binding,
-                                                 struct zmk_behavior_binding_event event) {
-    return ZMK_BEHAVIOR_OPAQUE;
+static const struct behavior_tapithium_mods_engine_data
+    tapithium_mods_engine_data = {
+        .stage = TP_STAGE_IDLE,
+        .mode = TP_MODE_ENABLE,
+        .config = NULL,
+        .pending_layer = 0,
+        .pending_mods = 0,
+};
+
+static int tapithium_mods_init(const struct device *dev) { return 0; };
+
+static int
+on_tapithium_mods_binding_pressed(struct zmk_behavior_binding *binding,
+                                  struct zmk_behavior_binding_event event) {
+  return ZMK_BEHAVIOR_OPAQUE;
 }
 
-static int on_tapithium_mods_binding_released(struct zmk_behavior_binding *binding,
-                                                  struct zmk_behavior_binding_event event) {
-    return ZMK_BEHAVIOR_OPAQUE;
+static int
+on_tapithium_mods_binding_released(struct zmk_behavior_binding *binding,
+                                   struct zmk_behavior_binding_event event) {
+  return ZMK_BEHAVIOR_OPAQUE;
 }
 
-// API struct
 static const struct behavior_driver_api tapithium_mods_driver_api = {
     .binding_pressed = on_tapithium_mods_binding_pressed,
     .binding_released = on_tapithium_mods_binding_released,
 };
 
-#define TAPITHIUM_MODS_INST(n)                                                                                       \
-    static struct behavior_tapithium_mods_data tapithium_mods_data_##n = {                                                                 \
-        .data_param1 = false,                                                                                             \
-        .data_param2 = false,                                                                                             \
-        .data_param3 = false,                                                                                             \
-    };                                                                                                                   \
-                                                                                                                         \
-    static struct behavior_tapithium_mods_config tapithium_mods_config_##n = {                                                               \
-        .config_param1 = false,                                                                                           \
-        .config_param2 = false,                                                                                           \
-        .config_param3 = false,                                                                                           \
-    };                                                                                                                   \
-                                                                                                                         \
-    BEHAVIOR_DT_INST_DEFINE(n,                                  /* Instance Number (Automatically populated by macro) */ \
-                            tapithium_mods_init,            /* Initialization Function */                            \
-                            NULL,                               /* Power Management Device Pointer */                    \
-                            &tapithium_mods_data_##n,       /* Behavior Data Pointer */                              \
-                            &tapithium_mods_config_##n,     /* Behavior Configuration Pointer */                     \
-                            POST_KERNEL,                        /* Initialization Level */                               \
-                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, /* Device Priority */                                    \
-                            &tapithium_mods_driver_api);    /* API struct */                                         \
+#define LAYER_BIT(node_id, prop, idx)                                          \
+  (1U << DT_PROP_BY_IDX(node_id, prop, idx)) |
+
+#define BUILD_LAYER_MASK(n, prop)                                              \
+  (DT_INST_FOREACH_PROP_ELEM(n, prop, LAYER_BIT) 0U)
+
+#define TAPITHIUM_MODS_INST(n)                                                 \
+  static struct behavior_tapithium_mods_data tapithium_mods_data_##n = {};     \
+                                                                               \
+  static struct behavior_tapithium_mods_config tapithium_mods_config_##n = {   \
+      .mod_layers = BUILD_LAYER_MASK(n, mod_layers),                           \
+      .cancel_after_idle_ms = DT_INST_PROP(n, cancel_after_idle_ms),           \
+  };                                                                           \
+                                                                               \
+  BEHAVIOR_DT_INST_DEFINE(                                                     \
+      n, /* Instance Number (Automatically populated by macro) */              \
+      tapithium_mods_init,        /* Initialization Function */                \
+      NULL,                       /* Power Management Device Pointer */        \
+      &tapithium_mods_data_##n,   /* Behavior Data Pointer */                  \
+      &tapithium_mods_config_##n, /* Behavior Configuration Pointer */         \
+      POST_KERNEL,                /* Initialization Level */                   \
+      CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, /* Device Priority */               \
+      &tapithium_mods_driver_api);         /* API struct */
 
 DT_INST_FOREACH_STATUS_OKAY(TAPITHIUM_MODS_INST)
 
